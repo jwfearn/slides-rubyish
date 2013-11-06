6a8b50642d2ce06c1a8401600d832d03f6cdf982
John Fearnside <john@seomoz.org>
2013-11-06 08:50:39 -0800

    C++11 compiler options

--- run.rb
+++ run.rb
@@ -3,9 +3,8 @@
 require 'tmpdir'
 
 src = ARGF.filename
-puts "about to compile and run #{src}"
 Dir.mktmpdir do |tmp|
 	exe = "#{tmp}/#{src}.exe"
-	opts = '-W -Wall -Werror -pedantic'
-	system("g++ #{opts} -o #{exe} #{src}") && system(exe)
+	opts = '-std=c++11 -stdlib=libc++ -Werror -pedantic -Wall -Wno-c++98-compat'
+	system("clang++ #{opts} -o #{exe} #{src}") && system(exe)
 end
