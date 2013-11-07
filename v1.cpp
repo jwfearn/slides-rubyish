d1f136abd4cab400d67be75241885c10641be61b
John Fearnside <john@seomoz.org>
2013-11-07 11:58:32 -0800

    Add reference

--- v1.cpp
+++ v1.cpp
@@ -3,7 +3,7 @@
 
 int main() {
   std::vector<int> v{-99, 0, 42, 3, -1, 6}; // uniform initialization
-  for (const auto & iter : v) { // auto, range-based for
+  for (const auto iter : v) { // auto, range-based for
     std::cout << iter << " ";
   }
   std::cout << std::endl;
